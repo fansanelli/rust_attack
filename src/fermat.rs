e604b180d14ceecd59ecae5d32e928ba4b0b5e8f
Francesco Ansanelli <francians@gmail.com>
2023-08-06 18:11:34 +0200

    Add fermat

---
+++ src/fermat.rs
@@ -0,0 +1,27 @@
+mod rust_attack_utils;
+use crate::rust_attack_utils::{isqrt_rem, is_congruent, is_square, isqrt};
+use num_bigint::BigInt;
+use num_traits::{One, Zero};
+
+
+fn main() {
+    println!("{}", fermat(BigInt::parse_bytes(b"94738740796943840961823530695778701408987757287583492665919730017973847138345511139064596113422435977583856843887008168202003855906708039013487349390571801141407245039011598810542232029634564848797998534872251549660454277336502838185642937637576121533945369150901808833844341421315429263207612372324026271327", 10).unwrap()));
+}
+
+fn fermat(n: BigInt) -> BigInt {
+    if is_congruent(&n, &BigInt::from(2), &BigInt::from(4)) {
+        return BigInt::zero();
+    }
+    let a_rem = isqrt_rem(&n);
+    let mut b2 = -a_rem.1;
+    let c0 = (a_rem.0 << 1u32) + BigInt::one();
+    let mut c = c0;
+    while !is_square(&b2) {
+        b2 += c.clone();
+        c += BigInt::from(2);
+    }
+    let a = (c - BigInt::one()) >> 1u32;
+    let b = isqrt(&b2);
+    return a - b;
+}
+
--- src/rust_attack_utils.rs
+++ src/rust_attack_utils.rs
@@ -1,5 +1,6 @@
-use num_bigint::BigInt;
-use num_traits::Zero;
+use num_bigint::{BigInt};
+use num_traits::{Zero, One};
+use std::ops::Add;
 
 #[inline]
 pub fn gcd(x: &BigInt, y: &BigInt) -> BigInt {
@@ -12,5 +13,55 @@ pub fn gcd(x: &BigInt, y: &BigInt) -> BigInt {
         n = temp;
     }
     n
+}
+
+#[inline]
+pub fn is_congruent(a: &BigInt, b: &BigInt, m: &BigInt) -> bool {
+    ((a.clone() - b.clone()) % m.clone()).is_zero()
+}
+
+#[inline]
+pub fn is_square(n: &BigInt) -> bool {
+    let root = isqrt(&n);
+    *n == root.clone() * root
+}
+
+#[test]
+fn is_square_works() {
+  assert_eq!(is_square(&BigInt::from(9)), true);
+  assert_eq!(is_square(&BigInt::from(10)), false);
+}
+
+#[inline]
+pub fn isqrt_rem(n: &BigInt) -> (BigInt, BigInt) {
+    let i2 = isqrt(&n);
+    (i2.clone(), n - (i2.clone() * i2))
+}
+
+#[test]
+fn isqrt_rem_works() {
+  let res = isqrt_rem(&BigInt::from(10));
+  assert_eq!(res.0, BigInt::from(3));
+  assert_eq!(res.1, BigInt::one());
+}
+
+#[inline]
+pub fn isqrt(n: &BigInt) -> BigInt {
+    if n.is_zero() {
+        return BigInt::zero();
+    }
+    let mut x: BigInt = n.clone();
+    let mut y: BigInt = (n.clone() + BigInt::one()) >> 1;
+    while y < x {
+    	x = y.clone();
+    	y = (y.clone() + n.clone() / y) >> 1;
+    }
+    x
+}
+
+#[test]
+fn isqrt_works() {
+  assert_eq!(isqrt(&BigInt::from(9)), BigInt::from(3));
+  assert_eq!(isqrt(&BigInt::from(10)), BigInt::from(3));
 }
 
